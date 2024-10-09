28a8f3d3b2fc35174bc8bca36cdf15c4a48e2a8d
Adiguna54 <aristaadiguna@gmail.com>
2024-10-09 20:06:38 +0800

    Second Commit

--- resources/views/crud/index.blade.php
+++ resources/views/crud/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 
@@ -12,6 +13,35 @@
     <h3>www.malasngoding.com</h3>
     <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
     <p>Ini adalah view blog. ada di route blog.</p>
+
+    <div class="py-4 mx-8 bg-lime-400">
+        <span class="font-bold text-3xl">Data Pegawai</span>
+
+        <a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
+
+        <div>
+            <tr>
+                <th>Nama</th>
+                <th>Jabatan</th>
+                <th>Umur</th>
+                <th>Alamat</th>
+                <th>Opsi</th>
+            </tr>
+
+            @foreach ($pegawais as $p)
+                <tr>
+                    <td>{{ $p->nama }}</td>
+                    <td>{{ $p->jabatan }}</td>
+                    <td>{{ $p->umur }}</td>
+                    <td>{{ $p->alamat }}</td>
+                    <td>
+                        <a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
+                        <a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
+                    </td>
+                </tr>
+            @endforeach
+        </div>
+    </div>
 </body>
 
 </html>
